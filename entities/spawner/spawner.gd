ca13dd57a861266ee7712422f981a837ff0e502d
unknown <wesleyray95@gmail.com>
2026-01-10 21:13:52 +0100

    basic box and spawner

---
+++ entities/spawner/spawner.gd
@@ -0,0 +1,16 @@
+class_name Spawner
+extends Node3D
+
+@export var trash_scene: PackedScene
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("debug1"):
+		print("debug1")
+		spawn_trash()
+
+
+func spawn_trash() -> void:
+	var t : Trash = trash_scene.instantiate()
+	t.trash_type = Trash.METAL
+	get_tree().root.add_child(t)
+	t.global_position = global_position
---
+++ entities/trash/trash.gd
@@ -0,0 +1,11 @@
+class_name Trash
+extends RigidBody3D
+
+enum {
+    METAL,
+    GLASS,
+    PLASTIC,
+    PAPER,
+}
+
+@export var trash_type := METAL
