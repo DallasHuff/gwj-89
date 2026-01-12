e19ce5a43531775f80c42a80b6b57de35c475132
unknown <wesleyray95@gmail.com>
2026-01-12 20:34:53 +0100

    added nearest distance grab to cursor

--- entities/components/interactor.gd
+++ entities/components/interactor.gd
@@ -11,11 +11,12 @@ var trash: Array[Trash] = []
 @onready var hold_spot: Marker3D = $HoldSpotMarker
 @onready var drop_spot: Marker3D = $DropSpotMarker
 @onready var drop_offset := Vector3(0.0, 0.5, 0.0)
-@onready var grab_area: Area3D = $GrabArea
+@onready var grab_marker: Marker3D = $GrabMarker
+@onready var grab_area: Area3D = $GrabMarker/GrabArea
 
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_pressed("pickup"):
+	if Input.is_action_just_pressed("pickup"):
 		pickup()
 	elif Input.is_action_just_pressed("putdown"):
 		putdown()
@@ -25,22 +26,31 @@ func pickup() -> void:
 
 	# move grab area
 	assert(is_instance_valid(player_camera))
-	grab_area.global_position = player_camera.mouse_position
+	grab_marker.global_position = player_camera.mouse_position + Vector3(0, 1.0, 0)
 
 	var bodies := grab_area.get_overlapping_bodies()
+	var closest_trash: Trash = null
+	var min_dist := INF
 	for i in range(bodies.size()):
 		if trash.size() >= inventory_size:
 			break
 		var body := bodies[i]
 		if body is Trash and body not in trash:
-			body = body as Trash
-			trash.append(body)
-			body.global_position = hold_spot.global_position + (TRASH_GAP * trash.size())
-			body.freeze = true
-			body.pickup_sfx.play()
-			body.reparent(self)
+
+			var dist_to_player := body.position.distance_to(grab_marker.position)
+			if min_dist > dist_to_player:
+				min_dist = dist_to_player
+				closest_trash = body as Trash
+
+	if closest_trash != null:
+		trash.append(closest_trash)
+		closest_trash.global_position = hold_spot.global_position + (TRASH_GAP * trash.size())
+		closest_trash.freeze = true
+		closest_trash.pickup_sfx.play()
+		closest_trash.reparent(self)
 	if trash.size() >= inventory_size:
 		AchievementTracker.achieve("Loaded Up")
+
 
 
 func putdown() -> void:
