a77b01225a28716ee56f65aa626efac29a76ecb5
DallasHuff <dallashuff45@gmail.com>
2026-01-12 21:19:23 -0600

    added some particle effects

--- entities/hopper/hopper.gd
+++ entities/hopper/hopper.gd
@@ -15,6 +15,9 @@ var goal_met := false
 @onready var zone := %Zone
 @onready var display := %Display
 @onready var damage_display := %DamageDisplay
+@onready var smoke: CPUParticles3D = %SmokeParticles
+@onready var sparks: CPUParticles3D = %SparkParticles
+@onready var blue_sparks: CPUParticles3D = %BlueSparkParticles
 
 func _on_zone_body_entered(body: Node3D) -> void:
 	if not body is Trash:
@@ -30,13 +33,16 @@ func _on_zone_body_entered(body: Node3D) -> void:
 	if got_trash_type != wanted_trash_type:
 		health -= damage_amount
 		damage_display.text = str(health) + "%"
+		sparks.emitting = true
 	else:
 		processed_count += 1
+		blue_sparks.emitting = true
 
 	EventsBus.trash_grinded.emit(got_trash_type, got_trash_type == wanted_trash_type)
 
 	if health <= 0:
 		destroyed = true
+		smoke.emitting = true
 		EventsBus.hopper_destroyed.emit()
 
 	display.text = str(processed_count)
