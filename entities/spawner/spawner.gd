6c22e1a3dddb3ca19192c48b7a041ab5350a82fa
unknown <wesleyray95@gmail.com>
2026-01-17 15:15:34 +0100

    updated spawner to have selectable random drop rates

--- entities/spawner/spawner.gd
+++ entities/spawner/spawner.gd
@@ -1,13 +1,28 @@
 class_name Spawner
 extends Node3D
 
-@export var trash_type := Trash.TrashType.PLASTIC
+# @export var trash_type := Trash.TrashType.PLASTIC
 @export var spawn_timer_range_min := 3.0
 @export var spawn_timer_range_max := 7.0
 @export var spawn_timer_start_delay := 3.0
 
+@export_category("Spawn Rates")
+	# METAL,
+	# GLASS,
+	# PLASTIC,
+	# PAPER,
+	# BODY,
+@export_range(0, 1) var metal_chance := 0.0
+@export_range(0, 1) var glass_chance := 0.0
+@export_range(0, 1) var plastic_chance := 0.0
+@export_range(0, 1) var paper_chance := 0.0
+@export_range(0, 1) var body_chance := 0.0 
+var all_chances := [metal_chance, glass_chance, plastic_chance, paper_chance, body_chance]
+
 var spawn_bounds_width := 0.3
 var spawn_timer: Timer
+
+var warning_pushed := false
 
 @onready var trash_scene := preload("uid://b6pgrrfecfoqx")
 
@@ -34,6 +49,36 @@ func _process(_delta: float) -> void:
 		spawn_trash()
 
 func spawn_trash() -> void:
+
+	# select random trash type
+	var sum_chances := metal_chance + glass_chance + plastic_chance + paper_chance + body_chance
+
+	if is_equal_approx(0, sum_chances):
+		if not warning_pushed:
+			push_warning("no trash type chances allotted, not spawning")
+			warning_pushed = true
+		return
+
+	var choice := randf_range(0, sum_chances)
+
+	var trash_type := Trash.TrashType.METAL
+
+	if choice > sum_chances - body_chance:
+		# print("choosing body")
+		trash_type = Trash.TrashType.BODY
+	elif choice > sum_chances - body_chance - paper_chance:
+		# print("choosing paper")
+		trash_type = Trash.TrashType.PAPER
+	elif choice > sum_chances - body_chance - paper_chance - plastic_chance:
+		# print("choosing plastic")
+		trash_type = Trash.TrashType.PLASTIC
+	elif choice > sum_chances - body_chance - paper_chance - plastic_chance - glass_chance:
+		# print("choosing glass")
+		trash_type = Trash.TrashType.GLASS
+	elif choice > sum_chances - body_chance - paper_chance - plastic_chance - glass_chance - metal_chance:
+		# print("choosing metal")
+		trash_type = Trash.TrashType.METAL
+
 	var random_pos_offset := Vector3.ZERO
 
 	random_pos_offset.x += randf_range(-spawn_bounds_width, spawn_bounds_width)
